507e9d1db3898cadc2213a8ab2e315f48395e895
Vinh <vjhnguyen@gmail.com>
2022-05-03 20:35:29 +0700

    Initial commit

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke(){
+  return "some jokes"
+}
+
+export default generateJoke
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import generateJoke from "./generateJoke";
+import './styles/main.scss'
+import avatar from './assets/avatar.jpg'
+
+const avatarImg = document.getElementById("avatar");
+avatarImg.src = avatar
+
+console.log(generateJoke());
